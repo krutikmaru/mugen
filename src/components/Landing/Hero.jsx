432bf657d389d3d6071ef88e1cca6143cebbfe65
krutikmaru <krutikmaru18@gmail.com>
2023-11-15 13:20:32 +0530

    Toast ✅

--- src/components/Landing/Hero.jsx
+++ src/components/Landing/Hero.jsx
@@ -1,7 +1,8 @@
 // Hero.jsx
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import Work from "./Work";
+import toast from "react-hot-toast";
 
 const Hero = () => {
   return (
@@ -9,7 +10,7 @@ const Hero = () => {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.5, duration: 0.8 }}
-      className="min-h-[50vh]  space-y-7 flex flex-col justify-center items-center absolute top-32 px-10 text-center"
+      className="min-h-[50vh]  space-y-7 flex flex-col justify-center items-center absolute top-32 px-10 text-center pb-32"
     >
       <HeroTitle />
       <ConnectWithUs />
@@ -46,6 +47,26 @@ const HeroTitle = () => {
 };
 
 const ConnectWithUs = () => {
+  const [email, setEmail] = useState("");
+  const handleConnectWithUsClick = () => {
+    if (!email) {
+      toast.error("Email is required", {
+        style: {
+          backgroundColor: "#080821",
+          color: "#fff",
+          border: "1px solid #131334",
+        },
+      });
+      return;
+    }
+    toast.success("We received you Mail", {
+      style: {
+        backgroundColor: "#080821",
+        color: "#fff",
+        border: "1px solid #131334",
+      },
+    });
+  };
   return (
     <motion.div
       initial={{ opacity: 0, y: 20 }}
@@ -56,9 +77,14 @@ const ConnectWithUs = () => {
       <input
         className="px-5 py-2 w-full sm:w-fit sm:mr-5 text-base font-light rounded-lg outline-none bg-[#ffffff0d] backdrop-blur-sm border-[1px] border-gray-800"
         type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
         placeholder="Enter your email..."
       ></input>
-      <button className="px-5 py-2 w-48 mt-7 sm:mt-0 bg-gradient-to-r text-base from-red-500 to-purple-600 rounded-lg hover:scale-105 transform transition-transform duration-300 ease-in-out">
+      <button
+        onClick={handleConnectWithUsClick}
+        className="px-5 py-2 w-48 mt-7 sm:mt-0 bg-gradient-to-r text-base from-red-500 to-purple-600 rounded-lg hover:scale-105 transform transition-transform duration-300 ease-in-out"
+      >
         Connect with us
       </button>
     </motion.div>
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import TopNavigation from "../TopNavigation/TopNavigation";
+import { Toaster } from "react-hot-toast";
 
 const Layout = ({ children }) => {
   return (
-    <div
-      className={`scrollclass bg-mugen-purple-dark min-h-screen w-screen font-lexend-deca text-white`}
-    >
-      <TopNavigation />
-      <div className="h-screen w-full mt-20 flex justify-center items-start overflow-x-hidden bg-mugen-purple-dark">
-        {children}
+    <>
+      <Toaster position="top-right" reverseOrder={false} />
+      <div
+        className={`scrollclass bg-mugen-purple-dark min-h-screen w-screen font-lexend-deca text-white`}
+      >
+        <TopNavigation />
+        <div className="h-screen w-full mt-20 flex justify-center items-start overflow-x-hidden bg-mugen-purple-dark">
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
