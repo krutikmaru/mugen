b60b03c1da76b2403b540c157288cd4ebf066a47
krutikmaru <krutikmaru18@gmail.com>
2023-12-11 19:12:45 +0530

    Contact popup fixed

--- src/components/Landing/MoreDetail.jsx
+++ src/components/Landing/MoreDetail.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useApplicationManager } from "../../contexts/ApplicationContext";
 import toast from "react-hot-toast";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const MoreDetail = (props) => {
   const [email, setEmail] = useState(props.email);
@@ -57,18 +59,18 @@ const MoreDetail = (props) => {
   return (
     <div
       onClick={(e) => e.stopPropagation()}
-      className="bg-gradient-to-r from-red-500 to-purple-600 rounded-lg p-[1px] min-h-fit  w-[50rem] mx-20 md:mx-40"
+      className="bg-[#16153b] rounded-lg p-[1px] min-h-fit  w-[50rem] mx-4 md:mx-40"
     >
       <div
         className="text-white min-h-full  w-full relative bg-mugen-purple-dark rounded-lg md:p-10 p-4  flex flex-col  space-y-6"
         style={{ paddingBottom: 180 }}
       >
         <div>
-          <label htmlFor="email" className="p-3 text-sm  text-gray-500">
+          <label htmlFor="email" className="text-sm  text-gray-500">
             Email
           </label>
           <input
-            className="mt-1 px-5 py-2 w-full text-base font-light rounded-lg outline-none bg-[#ffffff0d] backdrop-blur-sm border-[1px] border-gray-800"
+            className="mt-1 px-5 py-2 w-full text-base font-light rounded-md outline-none bg-[#ffffff0d] backdrop-blur-sm border-[1px] border-gray-800"
             type="email"
             id="email"
             value={email}
@@ -77,16 +79,15 @@ const MoreDetail = (props) => {
           ></input>
         </div>
         <div>
-          <label htmlFor="note" className="p-3 text-sm  text-gray-500">
+          <label htmlFor="note" className="text-sm  text-gray-500">
             Note
           </label>
           <textarea
-            className="mt-1 px-5 py-2 w-full min-h-28 max-h-32 text-base font-light rounded-lg outline-none bg-[#ffffff0d] backdrop-blur-sm border-[1px] border-gray-800"
+            className="mt-1 px-5 py-2 w-full min-h-28 max-h-32 text-base font-light rounded-md outline-none bg-[#ffffff0d] backdrop-blur-sm border-[1px] border-gray-800"
             type="text"
             id="note"
             value={note}
             onChange={(e) => setNote(e.target.value)}
-            placeholder="Note......"
           ></textarea>
         </div>
         <div className=" text-sm text-gray-500">
@@ -95,7 +96,7 @@ const MoreDetail = (props) => {
           </label>
           <div className="mt-2 flex md:justify-between flex-col md:flex-row md:space-x-3 pt-1">
             {services.map((service, index) => (
-              <label key={index} className="flex items-center space-x-2">
+              <label key={index} className="flex items-center space-x-2 mb-2">
                 <input
                   type="checkbox"
                   className="hidden"
@@ -103,9 +104,11 @@ const MoreDetail = (props) => {
                   checked={selectedServices.includes(service)}
                   onChange={() => handleCheckboxChange(index)}
                 />
-                <div className="relative w-4 h-4 border-2 border-purple-500 rounded-md cursor-pointer">
+                <div className="relative w-6 h-6 border-[1px]  border-purple-500 rounded-sm cursor-pointer">
                   {selectedServices.includes(service) && (
-                    <div className="w-full h-full bg-purple-500 "></div>
+                    <div className="w-full h-full bg-purple-500 flex justify-center items-center p-2">
+                      <FontAwesomeIcon icon={faCheck} className="text-white " />
+                    </div>
                   )}
                 </div>
                 <span className="pl-2">{service}</span>
